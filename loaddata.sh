30656c852b51a2034f366a98cc93b10587f119a7
tyayers <tyler.ayers@gmail.com>
2023-08-09 11:01:58 +0200

    first version

--- loaddata.sh
+++ loaddata.sh
@@ -5,7 +5,7 @@ export COUNTER=0
 
 while [ $COUNTER -le 10 ]
 do
-    RESULT=$(curl --location --request POST "https://firestore.googleapis.com/v1/projects/$GOOGLE_CLOUD_PROJECT/databases/(default)/documents/notes?documentId=b78dfdb" \
+    RESULT=$(curl --location --request POST "https://firestore.googleapis.com/v1/projects/$GOOGLE_CLOUD_PROJECT/databases/(default)/documents/inspections?documentId=b78dfdb" \
     --header "Authorization: Bearer $TOKEN" \
     --header "Content-Type: application/json" \
     --data-raw "{
