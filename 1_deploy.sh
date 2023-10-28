8408acd8dc695a1f53a14f1de59d94c4b879a1b0
Tyler Ayers <tyayers@google.com>
2023-10-28 06:22:32 +0200

    fixed enable drive bug

--- 1_deploy.sh
+++ 1_deploy.sh
@@ -2,6 +2,11 @@ export PROJECT=$(gcloud config get-value project)
 export LOCATION=europe-west1
 export NAME=inspectionsvc
 
+gcloud services enable appengine.googleapis.com
+gcloud services enable firestore.googleapis.com
+gcloud services enable aiplatform.googleapis.com
+gcloud services enable drive.googleapis.com
+
 gcloud builds submit --tag "eu.gcr.io/$PROJECT/$NAME"
 
 gcloud run deploy $NAME --image eu.gcr.io/$PROJECT/$NAME \
