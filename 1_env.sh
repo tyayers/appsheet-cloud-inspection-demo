d586d88e196c6a65fe174f3d28390d5c459228bb
tyayers <tyler.ayers@gmail.com>
2024-03-06 06:03:18 +0100

    fixed bug in build script

---
+++ 1_env.sh
@@ -0,0 +1,3 @@
+export PROJECT=$(gcloud config get-value project)
+export REGION=europe-west1
+export NAME=safety-vision-service
---
+++ 2_deploy.sh
@@ -0,0 +1,32 @@
+gcloud services enable appengine.googleapis.com
+gcloud services enable firestore.googleapis.com
+gcloud services enable artifactregistry.googleapis.com
+gcloud services enable cloudbuild.googleapis.com
+gcloud services enable run.googleapis.com
+gcloud services enable aiplatform.googleapis.com
+gcloud services enable documentai.googleapis.com
+gcloud services enable drive.googleapis.com
+
+# Create artifact registry, if needed
+gcloud artifacts repositories create docker-registry --repository-format=docker \
+--location="$REGION" --description="Docker registry" 2>/dev/null
+
+# Create firebase db and give service account rights, if needed
+if [[ $(gcloud app describe 2>&1 || true) == *'ERROR'* ]]; then echo 'No app engine or firestore instances found, creating...' && gcloud app create --region=europe-west; fi
+gcloud alpha firestore databases update --type=firestore-native
+PROJECTNUMBER=$(gcloud projects describe $PROJECT --format="value(projectNumber)")
+gcloud projects add-iam-policy-binding $PROJECT --member="serviceAccount:$PROJECTNUMBER-compute@developer.gserviceaccount.com" --role='roles/datastore.user'
+gcloud projects add-iam-policy-binding $PROJECT --member="serviceAccount:$PROJECTNUMBER-compute@developer.gserviceaccount.com" --role='roles/aiplatform.user'
+gcloud projects add-iam-policy-binding $PROJECT --member="serviceAccount:$PROJECTNUMBER-compute@developer.gserviceaccount.com" --role='roles/documentai.apiUser'
+echo "Add user $PROJECTNUMBER-compute@developer.gserviceaccount.com to your AppSheet Google Drive folder with Read permissions."
+
+# Submit build
+gcloud builds submit --config=cloudbuild.yaml \
+  --substitutions=_LOCATION="$REGION",_REPOSITORY="docker-registry",_IMAGE="$NAME" .
+
+# Deploy
+gcloud run deploy $NAME --image $REGION-docker.pkg.dev/$PROJECT/docker-registry/$NAME \
+    --platform managed --project $PROJECT \
+    --min-instances=1 \
+    --region $REGION --allow-unauthenticated \
+    --set-env-vars GCLOUD_PROJECT="$PROJECT"
